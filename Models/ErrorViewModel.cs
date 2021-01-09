0615a5b39d1e13bd5929aa9ece3cc2374e9c46e7
Northon T. Cavazzoni <northoncavazzoni@hotmail.com>
2021-01-08 21:20:27 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestorDeCarteira.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
